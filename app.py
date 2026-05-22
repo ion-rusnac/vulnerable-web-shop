726cfb1ef888ba5a4fbc358c18637303f65241df
Ion Rusnac <ion.rusnac@icloud.com>
2026-05-22 16:57:49 +0300

    Add: A minimal aproach for the Jinja template in index.html
    Signed-off-by: Ion Rusnac <ion.rusnac@icloud.com>

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ app = Flask(__name__)
 @app.route("/", methods=["GET"])
 def index():
     if request.method == "GET":
-        return render_template("index.html")
+        return render_template("main.html")
